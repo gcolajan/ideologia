c28593283cd5073f8cfcfc7cc0f0a0ed0073f99d
gcolajan <colag.contact@gmail.com>
2013-08-11 22:07:59 +0200

    Traduction du client RoR en PHP : récupération des fichiers et création de la base

---
+++ client/inclusions.php
@@ -0,0 +1,30 @@
+<?php 
+
+function entete($titre='Ideologia') {
+	echo '<!DOCTYPE html>
+	<html>
+	<head>
+		<title>'.$titre.' - Ideologia</title>
+		
+		<link rel="stylesheet" href="assets/stylesheets/application.css" />
+		<link rel="stylesheet" href="assets/stylesheets/plateau.css" />
+		<link rel="stylesheet" href="assets/stylesheets/notification.css" />
+		<link rel="stylesheet" href="assets/stylesheets/panel.css" />
+		<link rel="stylesheet" href="assets/stylesheets/pages.css" />
+		
+		<script language="Javascript" src="assets/javascripts/affichage.js></script>
+		<script language="Javascript" src="assets/javascripts/rgb.js></script>
+		<script language="Javascript" src="assets/javascripts/outils.js></script>
+		<script language="Javascript" src="assets/javascripts/timer.js></script>
+		<script language="Javascript" src="assets/javascripts/routeur.js></script>
+		<script language="Javascript" src="assets/javascripts/controleurs.js></script>
+		<script language="Javascript" src="assets/javascripts/WebsocketClass.js></script>
+	</head>
+	<body>';
+}
+
+function pied() {
+	echo '</body>';
+	echo '</html>';
+}
+?>
