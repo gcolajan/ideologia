1cdeb5fd26cc84e4c6e4f2463331a118c3a07960
gcolajan <colag.contact@gmail.com>
2013-08-12 12:34:02 +0200

    Correction des inclusions des fichiers JS (quotes de fin) et ajout meta UTF8

--- client/inclusions.php
+++ client/inclusions.php
@@ -6,19 +6,21 @@ function entete($titre='Ideologia') {
 	<head>
 		<title>'.$titre.' - Ideologia</title>
 		
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
 		<link rel="stylesheet" href="assets/stylesheets/application.css" />
 		<link rel="stylesheet" href="assets/stylesheets/plateau.css" />
 		<link rel="stylesheet" href="assets/stylesheets/notification.css" />
 		<link rel="stylesheet" href="assets/stylesheets/panel.css" />
 		<link rel="stylesheet" href="assets/stylesheets/pages.css" />
 		
-		<script language="Javascript" src="assets/javascripts/affichage.js></script>
-		<script language="Javascript" src="assets/javascripts/rgb.js></script>
-		<script language="Javascript" src="assets/javascripts/outils.js></script>
-		<script language="Javascript" src="assets/javascripts/timer.js></script>
-		<script language="Javascript" src="assets/javascripts/routeur.js></script>
-		<script language="Javascript" src="assets/javascripts/controleurs.js></script>
-		<script language="Javascript" src="assets/javascripts/WebsocketClass.js></script>
+		<script language="Javascript" src="assets/javascripts/affichage.js"></script>
+		<script language="Javascript" src="assets/javascripts/rgb.js"></script>
+		<script language="Javascript" src="assets/javascripts/outils.js"></script>
+		<script language="Javascript" src="assets/javascripts/timer.js"></script>
+		<script language="Javascript" src="assets/javascripts/routeur.js"></script>
+		<script language="Javascript" src="assets/javascripts/controleurs.js"></script>
+		<script language="Javascript" src="assets/javascripts/WebsocketClass.js"></script>
 	</head>
 	<body>';
 }
